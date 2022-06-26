acbacd04b7ee1d8e7bbc4792849c72f85611bf45
HKS <hks.online@gmail.com>
2022-06-26 12:28:55 -0400

    creates DAO class

--- tennis-player/src/main/java/com/example/springdata/tennisplayer/Player.java
+++ tennis-player/src/main/java/com/example/springdata/tennisplayer/Player.java
@@ -8,4 +8,65 @@ public class Player {
     private String nationality;
     private Date birthDate;
     private int titles;
+
+    public Player(){}
+
+    public Player(int id, String name, String nationality, Date birthDate, int titles){
+        this.id = id;
+        this.name = name;
+        this.nationality = nationality;
+        this.birthDate = birthDate;
+        this.titles = titles;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public int getTitles() {
+        return titles;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public void setTitles(int titles) {
+        this.titles = titles;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", nationality='" + nationality + '\'' +
+                ", birthDate=" + birthDate +
+                ", titles=" + titles +
+                '}';
+    }
 }
---
+++ tennis-player/src/main/java/com/example/springdata/tennisplayer/PlayerDao.java
@@ -0,0 +1,7 @@
+package com.example.springdata.tennisplayer;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PlayerDao {
+}

2bb7ea585b03f8c289941260aa2cf5df20cb2cb6
HKS <hks.online@gmail.com>
2022-06-26 12:18:02 -0400

    defines Player bean

---
+++ tennis-player/src/main/java/com/example/springdata/tennisplayer/Player.java
@@ -0,0 +1,11 @@
+package com.example.springdata.tennisplayer;
+
+import java.util.Date;
+
+public class Player {
+    private  int id;
+    private String name;
+    private String nationality;
+    private Date birthDate;
+    private int titles;
+}

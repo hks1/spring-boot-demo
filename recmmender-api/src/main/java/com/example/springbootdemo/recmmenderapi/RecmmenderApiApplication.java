139103963b0202830be13aa8f88abf44d12af325
HKS <hks.online@gmail.com>
2022-06-26 11:17:41 -0400

    Actuator endpoints

--- recmmender-api/src/main/java/com/example/springbootdemo/recmmenderapi/RecmmenderApiApplication.java
+++ recmmender-api/src/main/java/com/example/springbootdemo/recmmenderapi/RecmmenderApiApplication.java
@@ -13,3 +13,38 @@ public class RecmmenderApiApplication {
 }
 
 // mvn spring-boot:run
+
+// curl -v http://localhost:8080/actuator | json_pp
+
+/*
+{
+   "_links" : {
+      "health" : {
+         "href" : "http://localhost:8080/actuator/health",
+         "templated" : false
+      },
+      "health-path" : {
+         "href" : "http://localhost:8080/actuator/health/{*path}",
+         "templated" : true
+      },
+      "self" : {
+         "href" : "http://localhost:8080/actuator",
+         "templated" : false
+      }
+   }
+}
+
+ */
+
+// curl -v http://localhost:8080/movies | json_pp
+
+// To enable the web exposure of all management endpoints, add the following line to application.properties:
+//management.endpoints.web.exposure.include=*
+
+//http://localhost:8081/actuator/metrics
+//http://localhost:8081/actuator/mappings
+
+// Since we have enabled the HAL browser, we can view it at http://localhost:8080/browser/index.html#
+
+///actuator/metrics
+///actuator/metrics/jvm.memory.used

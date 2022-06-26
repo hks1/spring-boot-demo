67a005e0199539c2ac879d2361671fb7280f2cee
HKS <hks.online@gmail.com>
2022-06-25 22:25:17 -0400

    Creating a REST Service

---
+++ recmmender-api/src/main/java/com/example/springbootdemo/recmmenderapi/RecmmenderApiApplication.java
@@ -0,0 +1,15 @@
+package com.example.springbootdemo.recmmenderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecmmenderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecmmenderApiApplication.class, args);
+	}
+
+}
+
+// mvn spring-boot:run
---
+++ recmmender-api/src/test/java/com/example/springbootdemo/recmmenderapi/RecmmenderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemo.recmmenderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecmmenderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
